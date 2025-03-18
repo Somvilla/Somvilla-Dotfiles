a675e375c31566c0b38d3da8f34a05e2e5540cd5
Gary Somerville <gsomerville@pm.me>
2025-03-18 23:32:59 +0000

    .

---
+++ scripts/launch-apps.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#hyprctl dispatch exec [workspace 4 silent] firefox
+hyprctl dispatch exec [workspace 5 silent] discord
+hyprctl dispatch exec [workspace 8 silent] env LUTRIS_SKIP_INIT=1 lutris lutris:rungameid/1
+hyprctl dispatch exec [workspace 7 silent] steam
+hyprctl dispatch exec [workspace 3 silent] spotube
+hyprctl dispatch exec [workspace 2 silent] code
+hyprctl dispatch exec [workspace 6 silent] wasistlos
+hyprctl dispatch exec [workspace 9 silent] proton-mail
+
