843db9a68df1835323892c5e2ca5dff30659ed50
Gary Somerville <gsomerville@pm.me>
2025-11-17 02:23:51 +0000

    feat: update color palette settings for starship and kitty to enable starship control

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,17 +2,22 @@ local wezterm = require("wezterm")
 local config = require("config")
 require("events")
 
--- Apply color scheme based on the WEZTERM_THEME environment variable
-local themes = {
-	nord = "Nord (Gogh)",
-	onedark = "One Dark (Gogh)",
-	catppuccin = "Catppuccin Mocha",
-	-- catppuccin = "Catppuccin Macchiato",
-	-- catppuccin = "Catppuccin Frappe",
-	-- catppuccin = "Catppuccin Latte",
-}
-local success, stdout, stderr = wezterm.run_child_process({ os.getenv("SHELL"), "-c", "printenv WEZTERM_THEME" })
-local selected_theme = stdout:gsub("%s+", "") -- Remove all whitespace characters including newline
-config.color_scheme = themes[selected_theme]
+-- Color scheme disabled to allow starship palette control
+-- WezTerm will use default ANSI colors that starship can control
+-- Uncomment below to re-enable WezTerm color schemes (will override starship colors)
+
+-- Color scheme removed to allow starship ANSI color control
+
+-- local themes = {
+-- 	nord = "Nord (Gogh)",
+-- 	onedark = "One Dark (Gogh)",
+-- 	catppuccin = "Catppuccin Mocha",
+-- 	-- catppuccin = "Catppuccin Macchiato",
+-- 	-- catppuccin = "Catppuccin Frappe",
+-- 	-- catppuccin = "Catppuccin Latte",
+-- }
+-- local success, stdout, stderr = wezterm.run_child_process({ os.getenv("SHELL"), "-c", "printenv WEZTERM_THEME" })
+-- local selected_theme = stdout:gsub("%s+", "") -- Remove all whitespace characters including newline
+-- config.color_scheme = themes[selected_theme]
 
 return config
