67511293467e9d31f74ae5e3836e2d609e20eeb4
Gary Somerville <gsomerville@pm.me>
2025-11-17 01:10:34 +0000

    updates

---
+++ .config/zsh/custom.zsh
@@ -0,0 +1,97 @@
+
+# Pipenv
+export PIPENV_VENV_IN_PROJECT=1
+
+# Pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+command -v pyenv >/dev/null && eval "$(pyenv init -)"
+
+# Poetry
+export PATH="$HOME/.local/bin:$PATH"
+
+# Starship
+export STARSHIP_CONFIG="$HOME/.config/starship/starship.toml"
+eval "$(starship init zsh)"
+[ -n "$STARSHIP_THEME" ] && starship config palette "$STARSHIP_THEME"
+
+# Git completion (Arch)
+if [ -f /usr/share/zsh/site-functions/git-completion.zsh ]; then
+  source /usr/share/zsh/site-functions/git-completion.zsh
+fi
+autoload -Uz compinit && compinit
+
+# Neovim as MANPAGER
+export MANPAGER='nvim +Man!'
+
+# fzf (Arch package)
+[ -f /usr/share/fzf/key-bindings.zsh ] && source /usr/share/fzf/key-bindings.zsh
+[ -f /usr/share/fzf/completion.zsh ] && source /usr/share/fzf/completion.zsh
+
+export FZF_CTRL_T_OPTS="
+  --preview 'bat -n --color=always {}'
+  --bind 'ctrl-/:change-preview-window(down|hidden|)'"
+export FZF_DEFAULT_COMMAND='rg --hidden -l ""'
+
+# fd - cd to selected directory (rename so it doesn't shadow fd command)
+cdf() {
+  local dir
+  dir=$(find ${1:-.} -path '*/\.*' -prune \
+                  -o -type d -print 2> /dev/null | fzf +m) &&
+  cd "$dir"
+}
+
+# fh - search in your command history and execute selected command
+fh() {
+  eval $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed 's/ *[0-9]* *//')
+}
+
+# zoxide - a better cd command
+eval "$(zoxide init zsh)"
+
+# Syntax highlighting (Arch)
+if [ -f /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]; then
+  source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+fi
+(( ${+ZSH_HIGHLIGHT_STYLES} )) || typeset -A ZSH_HIGHLIGHT_STYLES
+ZSH_HIGHLIGHT_STYLES[path]=none
+ZSH_HIGHLIGHT_STYLES[path_prefix]=none
+
+# Autosuggestions (Arch)
+if [ -f /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh ]; then
+  source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+fi
+
+# Vi mode
+bindkey -v
+export KEYTIMEOUT=1
+export VI_MODE_SET_CURSOR=true
+
+function zle-keymap-select {
+  if [[ ${KEYMAP} == vicmd ]]; then
+    echo -ne '\e[2 q' # block
+  else
+    echo -ne '\e[6 q' # beam
+  fi
+}
+zle -N zle-keymap-select
+
+zle-line-init() {
+  zle -K viins
+  echo -ne '\e[6 q'
+}
+zle -N zle-line-init
+echo -ne '\e[6 q'
+
+# Yank to system clipboard (Wayland)
+function vi-yank-clipboard {
+  zle vi-yank
+  echo "$CUTBUFFER" | wl-copy
+}
+zle -N vi-yank-clipboard
+bindkey -M vicmd 'y' vi-yank-clipboard
+
+# Press 'v' in normal mode to launch $EDITOR with current line
+autoload edit-command-line
+zle -N edit-command-line
+bindkey -M vicmd v edit-command-line
